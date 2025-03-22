1a62d0aed87f8965aa8c991d10a057845b3abaa0
wellshsu <wellshsu@github.com>
2025-03-22 20:27:44 +0800

    test: 新增测试代码

--- Editor/Scripts/XEditor.Binary.cs
+++ Editor/Scripts/XEditor.Binary.cs
@@ -16,6 +16,7 @@ using UnityEditor.Build.Profile;
 #endif
 using UnityEditor.Build.Reporting;
 using EP.U3D.UTIL;
+using System.Runtime.InteropServices;
 
 namespace ET.U3D.UTIL
 {
@@ -518,6 +519,17 @@ namespace ET.U3D.UTIL
                             brustSrc = XFile.PathJoin(Output, Name, Application.productName + "_BurstDebugInformation_DoNotShip");
                         }
                         symbolSrc = XFile.PathJoin(Output, Name, bin + "_BackUpThisFolder_ButDontShipItWithYourGame");
+
+                        foreach (var dir in dirs)
+                        {
+                            XLog.Notice("XEditor.Binary.Process.Symbol.Dir: {0}", dir);
+                        }
+
+                        var files = Directory.GetFiles(Output);
+                        foreach (var file in files)
+                        {
+                            XLog.Notice("XEditor.Binary.Process.Symbol.File: {0}", file);
+                        }
                     }
                 }
                 else if (XEnv.Platform == XEnv.PlatformType.Android)
